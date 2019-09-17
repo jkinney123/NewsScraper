e74d06682f5d7bafcec91d085a4b892356da0c64
Joe Kinney <joekinney53@gmail.com>
2019-09-17 16:27:47 -0500

    fixed packag.json file

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var app = express();
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 // Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
